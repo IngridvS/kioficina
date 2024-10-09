f91de3fad46fc0696df11adf55d5d818d0327b9f
Ingrid Vitória Santos <142261730+IngridvS@users.noreply.github.com>
2024-10-09 15:01:16 +0000

    add. servicos

--- js/oficina.js
+++ js/oficina.js
@@ -1,30 +1,12 @@
-const swiper = new Swiper('.swiper', {
-    pagination: {
-        speed: 600,
-        parallax: true,
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-   
-//     right: var(--swiper-pagination-right, 8px);
-//     left: var(--swiper-pagination-left, auto);
-//     top: 50%;
-//     transform: translate3d(0px, -29%, 0);
-// }
-
-
-
-  
+$(document).ready(function () {
+  $('.banner div').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots:true,
+  });
 });
+
+
+  
