9e87d8753e2fed25631e8fe808e2455de8e35c7c
IngridvS <ingrid4633@gmail.com>
2024-11-17 20:26:16 -0300

    coloca os elementos no servico e o select em ordem

--- app/controllers/ServicoController.php
+++ app/controllers/ServicoController.php
@@ -1,11 +1,25 @@
 <?php
 
-class ServicoController extends Controller{
+class ServicoController extends Controller
+{
 
-    public function index(){
+    public function index()
+    {
         $dados = array();
-        $dados ['titulo'] = 'Serviços - Ki Oficina';
+        $dados['titulo'] = 'Serviços - Ki Oficina';
 
-        $this->carregarViews('servico',$dados);
+        //Instanciar o modelo servico Two
+        $servicoModel = new Servico();
+        $servicoOrdem = $servicoModel->getServicosOrdem();  //busca
+        
+        // Selecionar um serviço aleatório
+        $servicoodens = $servicoOrdem[array_rand($servicoOrdem)]; // Pega um serviço aleatório  
+        $dados['servico'] = $servicoodens;
+        
+
+        //var_dump($servicoOrdem);
+
+
+        $this->carregarViews('servico', $dados);
     }
 }
--- app/models/Servico.php
+++ app/models/Servico.php
@@ -1,8 +1,10 @@
 <?php
 
-class Servico extends Model{
+class Servico extends Model
+{
     //METODO PARA PEGAR SOMENTE 3 SERVICOS DE FORMA ALEATORIA
-    public function getServicoAleatorio($limite = 3){
+    public function getServicoAleatorio($limite = 3)
+    {
         $sql = "select * from tbl_servico where status_servico = 
         'Ativo' order by rand() limit :limite";
         $stmt = $this->db->prepare($sql);
@@ -11,13 +13,18 @@ class Servico extends Model{
         //PDOStatement::bindValue — Vincula um valor a um parâmetro
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-
 
     }
     //metodo listas tds os serviços ativos por ordem alfabetica 
 
-    // public function getServicoAleatorio{
-    //     $sql= "select * from "
-    // }
+    public function getServicosOrdem() {// () não recebe nenhum tipo de parametro como acima
+   
+        $sql = "select nome_servico, descricao_servico from tbl_servico where status_servico = 'Ativo' 
+        ORDER BY nome_servico;";
+         $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //fetchAll — Busca as linhas restantes de um conjunto de resultados
+        //PDO::FETCH_ASSOC: retorna uma matriz indexada pelo nome da coluna conforme retornado no seu conjunto de resultados
+    }
 }
